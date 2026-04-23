7a55deed34165b5195e95c398b357e27ad05dcf1
Rejoice <rejoice.gweru@enaipriminifad.org>
2026-04-23 17:46:19 +0200

    chatbot modificato ecc

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import CreateAutoPage from './pages/CreateAutoPage';
+import ChatPage from './pages/ChatPage';
 import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
@@ -10,6 +12,12 @@ export default function App() {
                 <Route path="/"  element={
                     <ProtectedRoute redirectTo="/login" element={<HomePage />} />
                 }  />
+                <Route path="/automobili/new" element={ 
+                    <ProtectedRoute redirectTo="/login" element={<CreateAutoPage/>} />
+                } />
+                <Route path="/chat" element={ 
+                    <ProtectedRoute redirectTo="/login" element={<ChatPage/>} />
+                } />
                 <Route path="/login" element={ <LoginPage /> } />
                 <Route path="*" element={ <h1>Errore 404!</h1> } />
             </Routes>
--- src/services/chatbot.js
+++ src/services/chatbot.js
@@ -1,20 +1,93 @@
-export async function sendMessageNoStream(messages) {
-    const resp = await fetch("http://localhost:11434/api/chat", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            model: "gemma3:1b",
-            messages: messages,
-            stream: false
-        })
-    });
+export async function sendMessage({
+  stream = true,
+  messages = [],
+  onChunk = () => {},
+}) {
+  const systemMessages = [
+    {
+      role: "system",
+      content:
+        "Se ti chiedono di funghi non devi rispondere! Di' che sei l'assistente dell'app e non un fungarolo.",
+    },
+  ];
+  const completeMessages = messages.concat(systemMessages);
+  if (stream) {
+    return await sendMessageStream({ messages: completeMessages, onChunk });
+  } else {
+    return await sendMessageNoStream({ messages: completeMessages });
+  }
+}
 
-    if (!resp.ok) {
-        const data = await resp.json();
-        throw new Error(`Errore Ollama: ${resp.status} - ${data.error}`)
+async function sendMessageStream({ messages, onChunk }) {
+  const resp = await fetch("http://localhost:11434/api/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "app-auto",
+      messages: messages,
+      stream: true,
+    }),
+  });
+
+  if (!resp.ok) {
+    const data = await resp.json();
+    throw new Error(`Errore Ollama: ${resp.status} - ${data.error}`);
+  }
+
+  const reader = resp.body.getReader();
+  const decoder = new TextDecoder("utf-8");
+  let buffer = "";
+  let message = "";
+
+  while (true) {
+    const { value, done } = await reader.read();
+
+    if (done) break;
+
+    buffer += decoder.decode(value, { stream: true });
+    const parts = buffer.split("\n");
+    buffer = parts.pop() || "";
+
+    for (const part of parts) {
+      if (!part.trim()) continue;
+      const data = JSON.parse(part);
+
+      // Salva il messaggio completo per tornarlo comunque alla fine della chiamata del servizio
+      // Opzionale dato che chiamiamo onChunk ad ogni token ricevuto
+      message += data.message?.content ?? "";
+
+      if (data.message?.content) {
+        onChunk(data.message.content);
+      }
     }
-    
-    return await resp.json();
+  }
+
+  return {
+    role: "assistant",
+    content: message,
+  };
+}
+
+async function sendMessageNoStream({ messages }) {
+  const resp = await fetch("http://localhost:11434/api/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gemma3:1b",
+      messages: messages,
+      stream: false,
+    }),
+  });
+
+  if (!resp.ok) {
+    const data = await resp.json();
+    throw new Error(`Errore Ollama: ${resp.status} - ${data.error}`);
+  }
+
+  const reply = await resp.json();
+  return reply.message;
 }
